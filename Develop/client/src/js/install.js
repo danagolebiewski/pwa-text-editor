73a6bd1886b7a24706f7038ba67a2845d6d6b79f
Dana Golebiewski <dana.golebiewski@gmail.com>
2022-11-01 16:09:53 -0600

    completed all todos - in install.js, webpackconfig, database.js, and src-sq.js files

---
+++ Develop/client/src/js/install.js
@@ -0,0 +1,26 @@
+const butInstall = document.getElementById("buttonInstall");
+
+// Logic for installing the PWA
+// TODO: Add an event handler to the `beforeinstallprompt` event
+window.addEventListener("beforeinstallprompt", (event) => {
+  event.preventDefault();
+  installBtn.style.visibility = "visible";
+  textHeader.textContent = "Click the button to install!";
+});
+
+// TODO: Implement a click event handler on the `butInstall` element
+butInstall.addEventListener("click", async () => {
+  const promptEvent = window.deferredPrompt;
+  if (!promptEvent) {
+    return;
+  }
+  promptEvent.prompt();
+  window.deferredPrompt = null;
+  butInstall.classList.toggle("hidden", true);
+});
+
+// TODO: Add an handler for the `appinstalled` event
+window.addEventListener("appinstalled", (event) => {
+  textHeader.textContent = "Successfully installed!";
+  console.log("👍", "appinstalled", event);
+});
---
+++ Develop/client/webpack.config.js
@@ -0,0 +1,62 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WebpackPwaManifest = require('webpack-pwa-manifest');
+const path = require('path');
+const { InjectManifest } = require('workbox-webpack-plugin');
+
+// TODO: Add and configure workbox plugins for a service worker and manifest file. - done 
+new GenerateSW(),
+new WebpackPwaManifest({
+  name: 'Text Editor',
+  short_name: 'text editor',
+  description: 'A place to edit text',
+  background_color: '#7eb4e2',
+  theme_color: '#7eb4e2',
+  start_url: './',
+  publicPath: './',
+  icons: [
+    {
+      src: path.resolve('Develop/client/src/images/logo.png'),
+      sizes: [96, 128, 192, 256, 384, 512],
+      destination: path.join('assets', 'icons'),
+    },
+  ],
+}),
+
+// TODO: Add CSS loaders and babel to webpack. - done 
+
+module.exports = () => {
+  return {
+    mode: 'development',
+    entry: {
+      main: './src/js/index.js',
+      install: './src/js/install.js'
+    },
+    output: {
+      filename: '[name].bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+    },
+    plugins: [
+      
+    ],
+
+    module: {
+      rules: [
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          },
+        },
+      ],
+    },
+  };
+};
