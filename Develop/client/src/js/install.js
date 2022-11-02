04fd1eaa34fccc929255dc092cc49a41b2a7e60a
Dana Golebiewski <dana.golebiewski@gmail.com>
2022-11-02 08:21:23 -0600

    removed comments and todos from files

--- Develop/client/src/js/install.js
+++ Develop/client/src/js/install.js
@@ -1,16 +1,11 @@
 const butInstall = document.getElementById("buttonInstall");
 
 // Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
 window.addEventListener("beforeinstallprompt", (event) => {
-  // event.preventDefault();
-  // installBtn.style.visibility = "visible";
   window.deferredPrompt = event;
   butInstall.classList.toggle('hidden', false);
-  // textHeader.textContent = "Click the button to install!";
 });
 
-// TODO: Implement a click event handler on the `butInstall` element
 butInstall.addEventListener("click", async () => {
   const promptEvent = window.deferredPrompt;
   if (!promptEvent) {
@@ -21,9 +16,6 @@ butInstall.addEventListener("click", async () => {
   butInstall.classList.toggle("hidden", true);
 });
 
-// TODO: Add an handler for the `appinstalled` event
 window.addEventListener("appinstalled", (event) => {
   window.deferredPrompt = null;
-  // textHeader.textContent = "Successfully installed!";
-  // console.log("👍", "appinstalled", event);
 });
--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -3,10 +3,6 @@ const WebpackPwaManifest = require("webpack-pwa-manifest");
 const path = require("path");
 const { InjectManifest } = require("workbox-webpack-plugin");
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file. - done
-// new GenerateSW(), - do I need this one?
-// TODO: Add CSS loaders and babel to webpack. - done
-
 module.exports = () => {
   return {
     mode: "development",
